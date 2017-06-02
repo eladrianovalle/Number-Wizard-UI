30e7f838cf35ea5d296b818e68f364e091d0f435
Adriano Valle <adrianovalle@me.com>
2017-06-02 11:30:09 -0400

    Level manager script

---
+++ Assets/_Project/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string level) {
+		Debug.Log ("Level load call for level: "+level);
+		SceneManager.LoadScene (level);
+
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("Quit request received!");
+		Application.Quit ();
+	}
+}
